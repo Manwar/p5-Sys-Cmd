c7ea82fdceb4df4c9878218907916fa313d5468e
Mark Lawrence <nomad@null.net>
2015-08-14 15:28:30 +0200

    Small cleanups

--- unexpected-termination.pl
+++ unexpected-termination.pl
@@ -4,15 +4,15 @@ use warnings;
 use lib 'lib';
 use Sys::Cmd 'spawn';
 
-#use Log::Any::Adapter 'Stdout';
+use Log::Any::Adapter 'Stdout';
 
 print "Spawning process\n";
 my $proc = spawn('file-not-found');
 
-$proc->stdin->print("x\n");
+#$proc->stdin->print("x\n");
 $proc->wait_child;
 
-print "Process ended with " . $proc->exit;
+print "Process ended with " . $proc->exit . "\n";
 
 END {
     print "END block\n";
